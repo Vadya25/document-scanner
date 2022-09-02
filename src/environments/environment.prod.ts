65d7a46851173ec56da32ab71f8f46640f35a87a
Vadya25 <filimonjuck_vadym@ukr.net>
2022-09-03 00:35:43 +0300

    product key in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  DWT_PRODUCT_KEY: 't0068WQAAAAv8FoXAw3mc0lLdL1wIFtG10s3ZUP9Q+H+QAeunloy5coktqRcohcBoe3dlA71p7W3h7Wga8q7sZeBURUSVs98'
 };
