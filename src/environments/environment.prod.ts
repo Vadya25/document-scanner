b577a9ac8140f2578a18e06a6e65c0fda65725e3
Vadya25 <filimonjuck_vadym@ukr.net>
2023-01-16 20:39:05 +0200

    api key for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  DWT_PRODUCT_KEY: 't0068WQAAAAv8FoXAw3mc0lLdL1wIFtG10s3ZUP9Q+H+QAeunloy5coktqRcohcBoe3dlA71p7W3h7Wga8q7sZeBURUSVs98'
+  DWT_PRODUCT_KEY: 't01529gIAAKQHDw6B1hUxb/w2mmyjmzT5ynrLqcFcvAqa7hRQZbYl5UzmjmrCzgb9f9qPfY/YHW6VhWMth28M16aX0GfR88VuqOeGERjMJvpRSafiDO/gZUAGgC9yBnbLD1jkcxpVcIAzIAmwbtyA65DfXnEQ0gPOgCTgOqQDQSe7PlPZBLSOkBQCzoAkYIR0oJU+E9X6Bsr6mj4='
 };
